e910190d10bb1dc8f86418e0168aa624ff95bd76
HP <lethuylinh232002@gmail.com>
2022-03-15 10:53:10 +0700

    kiemtraguaki

---
+++ app/src/main/java/com/example/kiemtragiuaki/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kiemtragiuaki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.login);
+    }
+   public  void login(View view){
+       Intent intent = new Intent(this,loginActivity.class);
+       this.startActivity(intent);
+   }
+
+}
---
+++ app/src/main/java/com/example/kiemtragiuaki/loginActivity.java
@@ -0,0 +1,4 @@
+package com.example.kiemtragiuaki;
+
+public class loginActivity {
+}
